32ca721187625399b9ac21790cfbe3f79c2b13de
Peter Milo <68414543+PeterMilo@users.noreply.github.com>
2023-07-30 08:38:52 +0200

    Add dynamic books via form

--- app.js
+++ app.js
@@ -1,41 +1,68 @@
-
 const addBookButton = document.querySelector('.add-book');
 const submitButton = document.querySelector('#submit-button');
 const bookList = document.querySelector('.book-list');
+const bookForm = document.querySelector('#book-form');
 
+// Variables to hold form values on submit
+let bookFormTitle = document.querySelector('input[placeholder="Book title"]');
+let bookFormAuthor = document.querySelector('input[placeholder="Author"]');
+let bookFormPages = document.querySelector(
+  'input[placeholder="Number of pages"]'
+);
+
+//Book library array
 let myLibrary = [];
 
 // Book constructor
-function Book (author, title, pages) {
-    this.author = author
-    this.title = title
-    this.pages = pages
+function Book(author, title, pages) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
 }
 
-// !!! This needs updating to dynamically add books from form
-function addBookToLibrary () {
-    const newBook = new Book ('Tolkien', 'The Hobbit', 324);
-    myLibrary.push(newBook);
-    updateLibraryDisplay();
+// Creates a new book object using the form variables
+function addBookToLibrary() {
+  const newBook2 = new Book('Tolkien', 'The Hobbit', 324);
+  const newBook = new Book(
+    `${bookFormAuthor.value}`,
+    `${bookFormTitle.value}`,
+    `${bookFormPages.value}`
+  );
+  myLibrary.push(newBook);
+  myLibrary.push(newBook2);
+  updateLibraryDisplay();
 }
 
 // When new book has been added to library, the library is updated in the HTML
-function updateLibraryDisplay () {
-    // Clear html list
-    bookList.innerHTML = ''
-    // Loop through the array
-    myLibrary.forEach((book) => {
-        // Create a list element for each book 
-        const addBook = document.createElement('li');
-        // Add content to the list item
-        addBook.textContent = `${book.title} written by ${book.author} has ${book.pages} pages`
-        // Add the list item to our html list
-        bookList.appendChild(addBook);
-    })
+function updateLibraryDisplay() {
+  // Clear html list
+  bookList.innerHTML = '';
+
+  // Loop through the array
+  myLibrary.forEach(book => {
+    // Create a list element for each book and adds the content
+    const addBook = document.createElement('li');
+    addBook.textContent = `${book.title} written by ${book.author} has ${book.pages} pages`;
+
+    // Add the list item to our html list
+    bookList.appendChild(addBook);
+  });
 }
 
+// Display form when Add book button is clicked
+addBookButton.addEventListener('click', () => {
+  bookForm.style.display = 'block';
+});
 
-addBookButton.addEventListener('click', addBookToLibrary)
-submitButton.addEventListener('click', function(event) {
-    event.preventDefault();
-})
+function clearForm() {
+  bookFormTitle.value = '';
+  bookFormAuthor.value = '';
+  bookFormPages.value = '';
+}
+
+submitButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  addBookToLibrary();
+  clearForm();
+  bookForm.style.display = 'none';
+});
